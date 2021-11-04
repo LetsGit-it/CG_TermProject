665df6b6d8bbda5b111e36cb996fa79c04cecbee
LeeSangWon <sswon314@naver.com>
2021-11-04 20:54:28 +0900

    Merge Pages
    시작 화면&게임 화면 연동

--- start-end.js
+++ start-end.js
@@ -1,4 +1,5 @@
-let scene, camera, renderer, skyboxGeo, skybox, controls, myReq;
+let scene, camera, renderer, controls;
+let floorGeometry, floorMaterial, floor, skyboxGeo, skybox, myReq;
 let zoomOut = false;
 let autoRotate = true;
 let flag = false;
@@ -31,41 +32,53 @@ var resultContent;
 window.onload = function init() {
     /* canvas를 쓰나 안쓰나 똑같이 innerWidth, innerHeight에 화면 띄움 */
     // html파일에 canvas태그를 사용할 때 사용
-    const canvas = document.getElementById("gl-canvas");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    // canvas = document.getElementById("gl-canvas");
+    // canvas.width = window.innerWidth;
+    // canvas.height = window.innerHeight;
 
-    const renderer = new THREE.WebGLRenderer({ canvas });
-    renderer.setSize(canvas.width, canvas.height);
+    // renderer = new THREE.WebGLRenderer({ canvas });
+    // renderer.setSize(canvas.width, canvas.height);
 
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xffffff);
+    // scene = new THREE.Scene();
+    // scene.background = new THREE.Color(0xffffff);
 
-    const camera = new THREE.PerspectiveCamera(55, canvas.width / canvas.height, 45, 30000);
-    camera.position.set(0, -250, 2000);
+    // camera = new THREE.PerspectiveCamera(55, canvas.width / canvas.height, 45, 30000);
+    // camera.position.set(0, -250, 2000);
 
     // html파일에 canvas를 안쓸 때 사용
-    // const scene = new THREE.Scene();
-    // const camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 45, 30000);
-    // camera.position.set(1200, -250, 2000);
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
+    camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 45, 3000);
+    camera.position.set(0, -250, 2000);
 
-    // const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    // renderer.setSize(window.innerWidth, window.innerHeight);
-    // renderer.domElement.id = "canvas";
-    // document.body.appendChild(renderer.domElement);
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.domElement.id = "canvas";
+    document.body.appendChild(renderer.domElement);
 
     // 배경 skybox 매트릭스 생성 및 scene에 추가함
     const materialArray = createMaterialArray(skyboxImage[skyboxImageIndex]);
 
-    skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000);
+    skyboxGeo = new THREE.BoxGeometry(1000, 1000, 1000);
     skybox = new THREE.Mesh(skyboxGeo, materialArray);
     scene.add(skybox);
+
+    // 바닥 생성
+    floorGeometry = new THREE.PlaneBufferGeometry(1000, 1000, 100, 100);
+    // floorGeometry.translate(0, 0, -2450);
+    floorMaterial = new THREE.MeshBasicMaterial();
+    floorMaterial.color.set("gray");
+
+    floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    floor.rotation.x = -Math.PI / 2;
+    floor.receiveShadow = true;
+    scene.add(floor);
 
     //controls 객체 생성 및 설정
     controls = new THREE.OrbitControls(camera, renderer.domElement);
     controls.enabled = flag; //true면 마우스로 배경 회전 가능 (시작, 끝 화면은 배경이 돌아가기만 하고 직접 돌릴 수 없음)
-    controls.minDistance = 700; //enable이 true일 때 마우스 휠로 줌인 줌 아웃 최대, 최소 거리 설정
-    controls.maxDistance = 1500;
+    // controls.minDistance = 700; //enable이 true일 때 마우스 휠로 줌인 줌 아웃 최대, 최소 거리 설정
+    controls.maxDistance = 150;
     controls.autoRotate = true;
     controls.autoRotateSpeed = 0.4; //배경화면 회전 속도
 
@@ -74,17 +87,17 @@ window.onload = function init() {
     zoomBtn = document.getElementById("zoom");
     loading = document.getElementById("loading");
 
-    mouseBtn.addEventListener("click", function () {
-        flag = !flag;
-        controls.enabled = flag;
-        mouseBtn.textContent = flag ? "Mouse On" : "Mouse Off";
-    });
-    autoRotateBtn.addEventListener("click", function () {
-        toggleAutoRotate(!autoRotate);
-    });
-    zoomBtn.addEventListener("click", function () {
-        toggleZoom(!zoomOut);
-    });
+    // mouseBtn.addEventListener("click", function () {
+    //     flag = !flag;
+    //     controls.enabled = flag;
+    //     mouseBtn.textContent = flag ? "Mouse On" : "Mouse Off";
+    // });
+    // autoRotateBtn.addEventListener("click", function () {
+    //     toggleAutoRotate(!autoRotate);
+    // });
+    // zoomBtn.addEventListener("click", function () {
+    //     toggleZoom(!zoomOut);
+    // });
 
     mapLeftBtn = document.getElementById("mapLeft");
     mapRightBtn = document.getElementById("mapRight");
@@ -122,14 +135,13 @@ window.onload = function init() {
     });
     startBtn.addEventListener("click", function () {
         document.getElementById("startId").style.display = "none";
-        document.getElementById("endId").style.display = "block";
+        document.getElementById("instructions").style.display = "-webkit-box";
 
-        //끝난 후 결과 보여주는 것
-        score = Math.floor(Math.random() * 1000) + 1;
-        var resultStr1 = "Score : " + score;
-        var resultStr2 = "Level : " + level[levelIndex];
-        resultContent.innerHTML = resultStr1 + "<br/>" + resultStr2;
+        //시작 설정 보여주는 것
         alert("Start!\nMap : " + mapContent.innerText + "\nLevel : " + levelContent.innerText);
+
+        gameStart();
+        return;
     });
 
     restartBtn.addEventListener("click", function () {
@@ -164,12 +176,12 @@ window.onload = function init() {
     function animate() {
         controls.autoRotate = autoRotate;
 
-        if (controls.maxDistance == 1500 && zoomOut) {
-            controls.maxDistance = 20000;
-            camera.position.z = 20000;
-        } else if (controls.maxDistance == 20000 && !zoomOut) {
-            controls.maxDistance = 1500;
+        if (controls.maxDistance == 150 && zoomOut) {
+            controls.maxDistance = 2000;
             camera.position.z = 2000;
+        } else if (controls.maxDistance == 2000 && !zoomOut) {
+            controls.maxDistance = 150;
+            camera.position.z = 200;
         }
         // console.log(camera.position);
 
